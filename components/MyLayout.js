e57bae1b4959bef4481a0e1f0560c83ebadaa4ab
Joost van der Griendt <joostvdg@gmail.com>
2019-12-28 22:32:49 +0100

    attempt at setting global background color
    Signed-off-by: Joost van der Griendt <joostvdg@gmail.com>

--- components/MyLayout.js
+++ components/MyLayout.js
@@ -6,6 +6,7 @@ export default function Layout(props) {
     return (
         <div>
             <Head>
+                <style>{'body { background-color: Cornsilk; }'}</style>
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <meta charSet="utf-8" />
                 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossOrigin="anonymous"></link>
